dd28d4194885d90de15ba37740b26e1e73d39011
Ahmet Ustun <austu@softserveinc.com>
2024-11-22 17:30:11 +0200

    Add job property and greet method to Vue instance and update HTML to display job and greeting

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "Ahmet",
+    job: "Developer",
+  },
+  methods: {
+    greet: function (time) {
+      return `Good ${time}, ${this.name}!`;
+    },
   },
 });
