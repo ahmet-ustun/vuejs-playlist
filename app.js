cc126d99e88208fe39968652ccafa6427ae85a5a
Ahmet Ustun <austu@softserveinc.com>
2024-12-17 14:20:08 +0200

    Add age property and methods to modify age in Vue instance; update HTML to display age and add event buttons

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ new Vue({
   data: {
     name: "Ahmet",
     job: "Developer",
+    age: 30,
   },
   methods: {
     greet: function (time) {
       return `Good ${time}, ${this.name}!`;
+    },
+    add: function (inc) {
+      this.age += inc;
+    },
+    subtract: function (dec) {
+      this.age -= dec;
     },
   },
 });
