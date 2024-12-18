9c9ebe53be0034854d3523f8d12fb45ef26ba952
Ahmet Ustun <austu@softserveinc.com>
2024-12-18 17:03:36 +0200

    Refactor Vue instances to create two separate applications; implement methods and computed properties for dynamic behavior; update HTML structure for multiple instance support

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-new Vue({
-  el: "#vue-app",
-  data: {},
+const one = new Vue({
+  el: "#vue-app-one",
+  data: {
+    title: "Vue App One",
+  },
   methods: {},
-  computed: {},
+  computed: {
+    greet: function () {
+      return "Hello from app one :)";
+    },
+  },
 });
+
+const two = new Vue({
+  el: "#vue-app-two",
+  data: {
+    title: "Vue App Two",
+  },
+  methods: {
+    changeTitle: function () {
+      one.title = "Title changed";
+    },
+  },
+  computed: {
+    greet: function () {
+      return "Yo dudes, this is app two speaking!";
+    },
+  },
+});
+
+two.title = "Changed from outside";
