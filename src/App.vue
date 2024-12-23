2d26b0d5dfc3d3108d3792ad1a9abc61654cd979
Ahmet Ustun <austu@softserveinc.com>
2024-12-23 18:09:14 +0200

    Replace Form components with AddBlog; update App.vue to include AddBlog and remove unused components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
   <div>
-    <keep-alive>
-      <component v-bind:is="component" />
-    </keep-alive>
-    <button v-on:click="component = 'form-one'">Show Form 1</button>
-    <button v-on:click="component = 'form-two'">Show Form 2</button>
+    <add-blog />
   </div>
 </template>
 
 <script>
-import FormHelper from "./components/FormHelper.vue";
-import FormOne from "./components/FormOne.vue";
-import FormTwo from "./components/FormTwo.vue";
+import AddBlog from './components/AddBlog.vue';
 
 export default {
   components: {
-    "form-helper": FormHelper,
-    "form-one": FormOne,
-    "form-two": FormTwo,
+    AddBlog,
   },
-  data() {
-    return { component: "form-one" };
-  },
+  data() {},
   methods: {},
 };
 </script>
