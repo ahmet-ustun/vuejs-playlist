70c729fdadd8bb1c873217fc4699abe8fbe432ee
Ahmet Ustun <austu@softserveinc.com>
2024-12-23 17:49:03 +0200

    Implement dynamic form switching; add FormOne and FormTwo components, and update App.vue to manage component state

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,28 @@
 <template>
   <div>
-    <form-helper>
-      <div slot="form-header">
-        <h3>This is the title of the form!</h3>
-        <p>Information about the form...</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="Name" required />
-        <input type="text" placeholder="Password" required />
-      </div>
-      <div slot="form-controls">
-        <button v-on:click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>
+    <keep-alive>
+      <component v-bind:is="component" />
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show Form 1</button>
+    <button v-on:click="component = 'form-two'">Show Form 2</button>
   </div>
 </template>
 
 <script>
 import FormHelper from "./components/FormHelper.vue";
+import FormOne from "./components/FormOne.vue";
+import FormTwo from "./components/FormTwo.vue";
 
 export default {
   components: {
     "form-helper": FormHelper,
+    "form-one": FormOne,
+    "form-two": FormTwo,
   },
   data() {
-    return {
-      title: "I am a dynamic title!",
-    };
+    return { component: "form-one" };
   },
-  methods: {
-    handleSubmit() {
-      alert("Form submitted!");
-    },
-  },
+  methods: {},
 };
 </script>
 
--- src/components/FormHelper.vue
+++ src/components/FormHelper.vue
@@ -25,6 +25,9 @@
 
 <script>
 export default {
+  components: {
+
+  },
   data() {
     return {};
   },
