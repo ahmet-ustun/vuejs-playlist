d12fa70d18bc2ed5c7f361775a95ad94267e1bb8
Ahmet Ustun <austu@softserveinc.com>
2024-12-27 19:29:29 +0200

    Add Header component and integrate it into App.vue for improved navigation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <app-header />
     <router-view />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 import AddBlog from "./components/AddBlog.vue";
 import ShowBlogs from "./components/ShowBlogs.vue";
 import ListBlogs from "./components/ListBlogs.vue";
 
 export default {
   components: {
+    'app-header': Header,
     AddBlog,
     ShowBlogs,
     ListBlogs,
